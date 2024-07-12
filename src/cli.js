3e96c6f9b9357269b057085534ca9099d4ca6dad
Teforch <matveev.gleb.boy@gmail.com>
2024-07-12 11:59:39 +0300

    3 step

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const welcomeUser = () => {
+	const name = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${name}!`);
+}
+export default welcomeUser
+
+

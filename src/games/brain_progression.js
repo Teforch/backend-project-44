ca9badae2ff4aafc17443bf0912d8a98e0776c67
Teforch <matveev.gleb.boy@gmail.com>
2024-07-15 12:44:19 +0300

    8 step

---
+++ src/games/brain_progression.js
@@ -0,0 +1,39 @@
+import getRandomNumber from '../getRandom.js'
+import index from '../index.js'
+
+export default () => {
+	const randomInteger = (min, max) => {
+		return Math.floor(Math.random() * (max - min + 1)) + min
+	}
+
+	const welcome = () => {
+		console.log('What number is missing in the progression?')
+	}
+
+	const valueForQuestion = () => {
+		const initialValue = getRandomNumber()
+		const amountOfNumbers = randomInteger(5, 10)
+		const valueOfProgression = randomInteger(1, 10)
+		const indexOfSecretValue = randomInteger(1, amountOfNumbers - 1)
+		let str = ``
+
+		for (let i = 1; i <= amountOfNumbers; i += 1) {
+			if (i === indexOfSecretValue) {
+				str += ` ..`
+				i++
+			}
+
+			str += ` ${initialValue + valueOfProgression * i}`
+		}
+		return str
+	}
+
+	const getCorrectAnswer = (expression) => {
+		const array = expression.split(' ')
+		const indexOfSecretValue = array.indexOf('..')
+		const valueOfProgression =
+			Number(array[array.length - 1]) - Number(array[array.length - 2])
+		return array[indexOfSecretValue + 1] - valueOfProgression
+	}
+	index(valueForQuestion, getCorrectAnswer, welcome)
+}
